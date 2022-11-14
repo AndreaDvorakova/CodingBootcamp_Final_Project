4fe2bd874343aa17fcd2001aaae419b5add44311
nadiiazimina <84657846+nadiiazimina@users.noreply.github.com>
2022-11-14 11:06:19 +0100

    final-version-database

--- database/migrations/2022_11_03_130751_create_users_table.php
+++ database/migrations/2022_11_03_130751_create_users_table.php
@@ -18,9 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email');
             $table->string('password');
-            $table->string('phone_number');
-            $table->string('lat')->nullable();
-            $table->string('long')->nullable();
+            $table->integer('telephone_number');
             $table->timestamps();
         });
     }
